0872dd07ed7a7273839543333f8f2f136dca2841
roxasventus <magicwand10@naver.com>
2022-06-25 16:00:02 +0900

    f

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import styled from 'styled-components'
+import "./Hello.css"
 
 function Hello(){
-    return <div>Hello World!</div>;
+    const PracticeStyle = {
+        marginTop: '10px',
+        backgroundColor: 'blue'
+    };
+
+    const StyledButton = styled.button`
+        color:red;
+        background-color:gray;
+    `;
+
+    return (
+        <>
+            <div style={{marginTop: '10px', backgroundColor: 'red'}}>Hello World!</div>
+
+            <div style= {PracticeStyle}>Hello World!</div>
+            <div style= {PracticeStyle}>Hello World!</div>
+            <div style= {PracticeStyle}>Hello World!</div>
+
+            <div className='red'>Hello World!</div>
+            <div className='red'>Hello World!</div>
+            <div className='red'>Hello World!</div>
+
+            <StyledButton>나만의 버튼</StyledButton>
+        </>
+    );
 }
 export default Hello;
